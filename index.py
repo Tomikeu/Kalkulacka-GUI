67ed0ffd90f0ce78cf2003114eecd50e03a198ed
Tomikeu <tomikeu66@gmail.com>
2023-12-14 19:30:07 +0100

    Neaky kody

--- index.py
+++ index.py
@@ -1,1 +1,15 @@
+import tkinter as tk
+from tkinter import ttk
 
+root = tk.Tk()
+root.title("Kalkulačka")
+root.geometry("500x600")
+root.resizable(False, False)
+#root.iconbitmap('')
+
+#Výběr znamének
+ 
+
+
+
+root.mainloop()
